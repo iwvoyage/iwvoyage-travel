fbdbc6b3fbca78f826493ad978e6c10fadda6b55
Luis Curbelo <luiscurbelo@Luiss-iMac.lan1>
2025-05-01 21:19:36 -0700

    Add render.com domain to ALLOWED_HOSTS

--- iwHOME/settings.py
+++ iwHOME/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Security
 SECRET_KEY = 'django-insecure-uzm_4#%u4t4#38k9_$a(sx$oq4%64n1q-i1cr2l$=09_a996r4'
 DEBUG = False
-ALLOWED_HOSTS = ['iwvoyage.pythonanywhere.com']
+ALLOWED_HOSTS = ['iwvoyage-travel.onrender.com']
 
 # Installed Applications
 INSTALLED_APPS = [
